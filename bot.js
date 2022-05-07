b31cbb86b4495375b8d7158614e41ce0445623ce
lev1athank <nik.parh.googl@gmail.com>
2022-05-08 02:40:54 +0500

    light

---
+++ bot.js
@@ -0,0 +1,73 @@
+var x = new XMLHttpRequest();
+const time = 2
+var id;
+function send_request(id = null) {
+    var url = "https://api.telegram.org/bot5104688774:AAFZr02MVqTE3shadMVbxQ9c-t9JIWOo_BE/getUpdates"
+    if(id) {
+        url += '?offset=' +id
+    }
+    x.open("GET", url, false);
+    x.send(null)
+    return JSON.parse(x.responseText)
+}
+
+function toggle_light(state) {
+    document.querySelector("#light").classList.toggle("light")
+}
+
+function send_user(text) {
+    if(text) {
+
+    }
+    
+}
+
+function readMessage() {
+    if(!id) {
+        list_message = send_request()
+        if(list_message['result'].length != 0) {
+            id = list_message['result'][list_message['result'].length-1]['update_id']
+        }
+        else 
+            return
+    }
+    var text = send_request(id)['result']
+
+    if(text.length != 0) {
+        id+=1;
+        switch (text[0]['message']['text'].toLowerCase()) {
+            case "вкл":
+                toggle_light(true)
+                break;
+        
+            case "включить":
+                toggle_light(true)
+                break;
+
+            case "выкл":
+                toggle_light(false)
+                break;
+        
+            case "выключить":
+                toggle_light(false)
+                break;
+                
+            default:
+                send_user("я тебя не понял")
+                break;
+        }
+    }
+}
+
+
+setInterval(()=>readMessage(), 1000 * time)
+
+
+
+console.log(123);
+
+//console.log(['result'][2]['message']['text']);
+
+//x.send(null);
+
+
